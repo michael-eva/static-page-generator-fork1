76b70c4041fc2f46838d5fbc4fb4c1083c51c1e6
galactic-shark <evamichael100@gmail.com>
2025-04-07 11:01:31 +0800

    build err

--- src/lib/supabase/server/supsbase.ts
+++ src/lib/supabase/server/supsbase.ts
@@ -1,11 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseServiceRoleKey =
-  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 if (
   !process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  !process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
+  !process.env.SUPABASE_SERVICE_ROLE_KEY
 ) {
   console.error("Missing required Supabase environment variables server");
   console.error(
@@ -13,8 +12,8 @@ if (
     process.env.NEXT_PUBLIC_SUPABASE_URL
   );
   console.error(
-    "NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY:",
-    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
+    "SUPABASE_SERVICE_ROLE_KEY:",
+    process.env.SUPABASE_SERVICE_ROLE_KEY
   );
 }
 if (!supabaseUrl || !supabaseServiceRoleKey) {
